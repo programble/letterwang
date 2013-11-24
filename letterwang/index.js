27262060d57618b16cb941a6a3553ec74e215e34
Curtis McEnroe <programble@gmail.com>
2013-11-24 02:18:35 -0500

    Add player pairing

--- letterwang/index.js
+++ letterwang/index.js
@@ -10,4 +10,37 @@ app.get('/', function(req, res) {
   res.sendfile(path.normalize(__dirname + '/../public/index.html'));
 });
 
+var nextId = 10000000,
+    players = {},
+    randomPool = [];
+
+function pairPlayers(player1, player2) {
+  player1.opponent = player2;
+  player2.opponent = player1;
+  player1.emit('opponent', player2.id);
+  player2.emit('opponent', player1.id);
+}
+
+io.sockets.on('connection', function(player) {
+  player.id = nextId.toString(36);
+  nextId++;
+  players[id] = player;
+
+  player.on('play random', function() {
+    if (randomPool.length) {
+      pairPlayers(player, randomPool.shift());
+    } else {
+      randomPool.push(player);
+    }
+  });
+
+  player.on('play id', function(id, fn) {
+    if (opponent = players[id]) {
+      pairPlayers(player, opponent);
+    } else {
+      fn('Player not found');
+    }
+  });
+});
+
 module.exports = server;
