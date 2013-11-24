2ffc839f5dbb745df631b7e5b587b6c783b23c01
Curtis McEnroe <programble@gmail.com>
2013-11-24 01:46:24 -0500

    Serve static content from public

--- letterwang/index.js
+++ letterwang/index.js
@@ -1,8 +1,13 @@
-var express = require('express'),
+var path    = require('path'),
+    express = require('express'),
     app     = express(),
     server  = require('http').createServer(app),
     io      = require('socket.io').listen(server);
 
 app.use(express.logger());
+app.use(express.static(path.normalize(__dirname + '/../public')));
+app.get('/', function(req, res) {
+  res.sendfile(path.normalize(__dirname + '/../public/index.html'));
+});
 
 module.exports = server;
